2db58aa8f6259322b6a024a293ddd2c5bfbdd06f
songminyoung <songmy1206@gmail.com>
2023-02-22 15:57:52 +0900

    230222 수업

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// 패널돌리기
+const frame = document.querySelector("section");
+const articleArr = frame.querySelectorAll("article");
+const len = articleArr.length;
+const deg = 360 / len;
+
+for(let i = 0; i<len; i++){
+  articleArr[i].style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
+}
+
